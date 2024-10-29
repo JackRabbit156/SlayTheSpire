ab385b4a137cb77645985c241d47b4815f9f29ee
Vladislav Keil <VladislavKeil@bundeswehr.org>
2024-10-29 11:11:49 +0100

    add: Card getPrices

--- src/models/cards/card_structure/Card.java
+++ src/models/cards/card_structure/Card.java
@@ -2,7 +2,11 @@ package models.cards.card_structure;
 
 import models.GameContext;
 
+import java.util.Random;
+
 public abstract class Card {
+    private Random rand = new Random();
+
     private String name;
     private String description;
     private int cost;
@@ -25,11 +29,29 @@ public abstract class Card {
         return name;
     }
 
-    public String getDescription(){
+    public String getDescription() {
         return description;
     }
 
-    public CardRarity getCardRarity(){
+    public CardRarity getCardRarity() {
         return cardRarity;
+    }
+
+    public int getPrice() {
+        // TODO - ShopController - One of the cards will always be On Sale, reducing its cost by 50%.
+        // TODO - ShopController - There will always be 2 Attack cards, 2 Skill cards, and 1 Power card.
+        // Existieren nur 3 Typen
+        // https://slay-the-spire.fandom.com/wiki/Merchant
+        switch (this.cardRarity) {
+            case COMMON: {
+                return rand.nextInt(55 + 1 - 45) + 45;
+            }
+            case UNCOMMON: {
+                return rand.nextInt(82 + 1 - 68) + 68;
+            }
+            case RARE: {
+                return rand.nextInt(165 + 1 - 135) + 135;
+            }
+        }
     }
 }
