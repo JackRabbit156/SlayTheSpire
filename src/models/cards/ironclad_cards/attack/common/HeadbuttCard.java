37b258fe03741c6142f4ee35f1b06410e4e09ea3
Daniel Willig <danielwillig@auf.bundeswehr.de>
2024-10-30 14:33:34 +0100

    finished Card

--- src/models/cards/ironclad_cards/attack/common/HeadbuttCard.java
+++ src/models/cards/ironclad_cards/attack/common/HeadbuttCard.java
@@ -1,12 +1,16 @@
 package models.cards.ironclad_cards.attack.common;
 
+import models.BattleDeck;
 import models.GameContext;
 import models.cards.card_structure.AttackCard;
+import models.cards.card_structure.Card;
 import models.cards.card_structure.CardGrave;
 import models.cards.card_structure.CardRarity;
 import models.enemy.Enemy;
 import models.player.player_structure.Player;
 
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class HeadbuttCard extends AttackCard {
@@ -22,10 +26,17 @@ public class HeadbuttCard extends AttackCard {
         Enemy enemy = gameContext.getEnemies().get(targetIndex);
         enemy.takeDamage(dealDamage());
 
+        BattleDeck battleDeck = gameContext.getBattleDeck();
+        List<Card> discardPile = battleDeck.getDiscardPile();
+
+        Random rand = new Random();
+        int targetCard = rand.nextInt(discardPile.size());
+
+        battleDeck.getDeck().add(discardPile.get(targetCard));
+
+
         Player player = gameContext.getPlayer();
         player.decreaseCurrentEnergy(getCost());
-
-        //TODO Put a card from your discard pile on top of your draw pile.
     }
 
     @Override
