b1f5b102f00f89d6fbd4ed354f160986dda2a2fd
Vladislav Keil <vladislavkeil@auf.bundeswehr.de>
2024-11-03 19:45:02 +0100

    Add: Loot Menu after a Win

--- src/models/map_elements/field_types/BossField.java
+++ src/models/map_elements/field_types/BossField.java
@@ -1,8 +1,10 @@
 package models.map_elements.field_types;
 
+import controller.LootViewController;
 import models.player.player_structure.Player;
 
 public class BossField extends Field{
+    private LootViewController lootViewController;
 
     public BossField() {
         super("\uD83D\uDCAA");
@@ -11,5 +13,9 @@ public class BossField extends Field{
     @Override
     public void doFieldThing(Player player) {
 
+
+        //TODO Add here LootViewController(player, FieldType)
+        lootViewController = new LootViewController(player, "BossField");
+        lootViewController.openLootView(player);
     }
 }
--- src/models/map_elements/field_types/EliteField.java
+++ src/models/map_elements/field_types/EliteField.java
@@ -1,8 +1,11 @@
 package models.map_elements.field_types;
 
+import controller.LootViewController;
 import models.player.player_structure.Player;
 
 public class EliteField extends Field {
+    private LootViewController lootViewController;
+
     public EliteField() {
         super("\uD83D\uDCA2");
     }
@@ -10,5 +13,10 @@ public class EliteField extends Field {
     @Override
     public void doFieldThing(Player player) {
 
+
+
+        //TODO Es muss geprüft werden, ob das Battle erfolgreich war.
+        lootViewController = new LootViewController(player, "EliteField");
+        lootViewController.openLootView(player);
     }
 }
