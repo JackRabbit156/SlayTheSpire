9bc2d3edb8bddf737ba4d59f62248297b128eba6
Marijan Loeschner <marijanloeschner@auf.bundeswehr.de>
2024-11-04 09:55:33 +0100

    removed playerName variable aufruf durch statischen kontext aus MainMenuViewController

--- src/controller/menus/CharacterMenuViewController.java
+++ src/controller/menus/CharacterMenuViewController.java
@@ -12,7 +12,6 @@ import view.menus.MainMenuView;
 import java.util.Scanner;
 
 public class CharacterMenuViewController {
-    public String playerName;
     private String selectedCharacter;
     private CharacterMenuView charView;
     private Scanner in = new Scanner(System.in);
@@ -24,7 +23,6 @@ public class CharacterMenuViewController {
     //Führt zurück zum Main Menu
     public void getBacktoMenu(){
         MainMenuViewController mainMenu = new MainMenuViewController();
-        playerName = MainMenuViewController.playerName;
         mainMenu.startMenu();
     }
     //Führt zurück zur Charakterauswahl
