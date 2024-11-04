722c5eac5d393f5edb5d54db1ac6b68f6fb759f1
Vladislav Keil <vladislavkeil@auf.bundeswehr.de>
2024-11-04 12:46:26 +0100

    REF: Main

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,8 @@
-import controller.BattleViewController;
-import controller.MapViewController;
 import controller.menus.MainMenuViewController;
-import helper.Color;
-import models.cards.DeckFactory;
-import controller.menus.MainMenuViewController;
-import models.enemy.Enemy;
-import models.player.Ironclad;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Ironclad player = new Ironclad();
-        MapViewController map = new MapViewController(player);
-
-        /*Ironclad player = new Ironclad();
-
-        List<Enemy> enemies = new ArrayList<Enemy>();
-
-        enemies.add(new AcidSlime());
-        enemies.add(new Cultist());
-        enemies.add(new SpikeSlime());
-
-        BattleViewController controller = new BattleViewController(player, enemies);
-        controller.startBattle();*/
+        MainMenuViewController menu = new MainMenuViewController();
+        menu.startMenu();
     }
 }
