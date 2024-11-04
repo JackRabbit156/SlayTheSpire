4e5019490c8157a9d7e0858bfef6785a470a0b52
Vladislav Keil <vladislavkeil@auf.bundeswehr.de>
2024-11-04 08:25:42 +0100

    Add: Getter & Setter GameSettings

--- src/models/game_settings/GameSettings.java
+++ src/models/game_settings/GameSettings.java
@@ -1,7 +1,26 @@
 package models.game_settings;
 
 import models.game_settings.structure.DifficultyLevel;
+import models.game_settings.structure.GameMode;
 
 public class GameSettings {
-    public static DifficultyLevel difficultylevel = null;
+    private static GameMode gameMode;
+    private static DifficultyLevel difficultyLevel;
+
+
+    public static GameMode getGameMode() {
+        return gameMode;
+    }
+
+    public static void setGameMode(GameMode gameMode) {
+        gameMode = gameMode;
+    }
+
+    public static DifficultyLevel getDifficultyLevel() {
+        return difficultyLevel;
+    }
+
+    public static void setDifficultyLevel(DifficultyLevel difficultyLevel) {
+        difficultyLevel = difficultyLevel;
+    }
 }
