10ecb9c5f638f2d4245b2b4eee1ca922099e6b1c
Daniel Willig <danielwillig@auf.bundeswehr.de>
2024-11-04 10:24:01 +0100

    finished unknown locations

--- src/models/map_elements/field_types/UnknownField.java
+++ src/models/map_elements/field_types/UnknownField.java
@@ -2,13 +2,31 @@ package models.map_elements.field_types;
 
 import models.player.player_structure.Player;
 
+import java.util.Random;
+
 public class UnknownField extends Field {
+    Random rand = new Random();
+    Field[] fields = {new EventField(), new EliteField(), new EnemyField(), new ShopField()};
+    int[] percentages = {67, 20, 10, 3};
+    int percentStart = 1;
+
     public UnknownField() {
         super("❓");
     }
 
     @Override
     public void doFieldThing(Player player) {
+        int percentEnd = 0;
+        for (int percentage : percentages) {
+            percentEnd += percentage;
+        }
 
+        int randomField = (rand.nextInt(percentEnd) + 1);
+        for (int i = 0; i < percentages.length; i++) {
+            if (percentStart <= randomField && randomField >= percentages[i]) {
+                fields[i].doFieldThing(player);
+            }
+            randomField -= percentages[i];
+        }
     }
 }
