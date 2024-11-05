0cdba71e90680aac15c66aa75e2febe258885a15
Daniel Willig <danielwillig@auf.bundeswehr.de>
2024-11-05 14:18:34 +0100

    added a bunch of Colors

--- src/helper/Color.java
+++ src/helper/Color.java
@@ -2,12 +2,25 @@ package helper;
 
 /**
  * @author Keil, Vladislav
+ * @author OF Daniel Willig
  */
 public enum Color {
     RESET {
         @Override
         public String toString() {
             return "\u001B[0m";
+        }
+    },
+    BOLD {
+        @Override
+        public String toString() {
+            return "\u001B[1m";
+        }
+    },
+    ITALIC {
+        @Override
+        public String toString() {
+            return "\u001B[3m";
         }
     },
     BLACK {
@@ -57,5 +70,155 @@ public enum Color {
         public String toString() {
             return "\u001B[37m";
         }
-    }
+    },
+    BLACKBRIGHT {
+        @Override
+        public String toString() {
+            return "\u001B[90m";
+        }
+    },
+    REDBRIGHT {
+        @Override
+        public String toString() {
+            return "\u001B[91m";
+        }
+    },
+    GREENBRIGHT {
+        @Override
+        public String toString() {
+            return "\u001B[92m";
+        }
+    },
+    YELLOWBRIGHT {
+        @Override
+        public String toString() {
+            return "\u001B[93m";
+        }
+    },
+    BLUEBRIGHT {
+        @Override
+        public String toString() {
+            return "\u001B[94m";
+        }
+    },
+    PURPLEBRIGHT {
+        @Override
+        public String toString() {
+            return "\u001B[95m";
+        }
+    },
+    CYANBRIGHT {
+        @Override
+        public String toString() {
+            return "\u001B[96m";
+        }
+    },
+    WHITEBRIGHT {
+        @Override
+        public String toString() {
+            return "\u001B[97m";
+        }
+    },
+    BLACKBACKGROUND {
+        @Override
+        public String toString() {
+            return "\u001B[40m";
+        }
+    },
+    REDBACKGROUND {
+        @Override
+        public String toString() {
+            return "\u001B[41m";
+        }
+    },
+    GREENBACKGROUND {
+        @Override
+        public String toString() {
+            return "\u001B[42m";
+        }
+    },
+    YELLOWBACKGROUND {
+        @Override
+        public String toString() {
+            return "\u001B[43m";
+        }
+    },
+    BLUEBACKGROUND {
+        @Override
+        public String toString() {
+            return "\u001B[44m";
+        }
+    },
+    PURPLEBACKGROUND {
+        @Override
+        public String toString() {
+            return "\u001B[45m";
+        }
+    },
+    CYANBACKGROUND {
+        @Override
+        public String toString() {
+            return "\u001B[46m";
+        }
+    },
+    GRAYBACKGROUND {
+        @Override
+        public String toString() {
+            return "\u001B[47m";
+        }
+    },
+    WHITEBACKGROUND {
+        @Override
+        public String toString() {
+            return "\u001B[48m";
+        }
+    },
+    BLACKBACKGROUNDBRIGHT {
+        @Override
+        public String toString() {
+            return "\033[0;100m";
+        }
+    },
+    REDBACKGROUNDBRIGHT {
+        @Override
+        public String toString() {
+            return "\033[0;101m";
+        }
+    },
+    GREENBACKGROUNDBRIGHT {
+        @Override
+        public String toString() {
+            return "\033[0;102m";
+        }
+    },
+    YELLOWBACKGROUNDBRIGHT {
+        @Override
+        public String toString() {
+            return "\033[0;103m";
+        }
+    },
+    BLUEBACKGROUNDBRIGHT {
+        @Override
+        public String toString() {
+            return "\033[0;104m";
+        }
+    },
+    PURPLEBACKGROUNDBRIGHT {
+        @Override
+        public String toString() {
+            return "\033[0;105m";
+        }
+    },
+    CYANBACKGROUNDBRIGHT {
+        @Override
+        public String toString() {
+            return "\033[0;106m";
+        }
+    },
+    WHITEBACKGROUNDBRIGHT {
+        @Override
+        public String toString() {
+            return "\033[0;107m";
+        }
+    },
 }
