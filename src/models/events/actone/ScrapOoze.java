19eabddac99fe0297b78941b0674816821b70602
Marijan Loeschner <marijanloeschner@auf.bundeswehr.de>
2024-11-06 12:35:23 +0100

    Events made foolproof

--- src/models/events/actone/ScrapOoze.java
+++ src/models/events/actone/ScrapOoze.java
@@ -30,12 +30,12 @@ public class ScrapOoze extends Event {
         EventView.displayHead(getTitle(), getStory());
         System.out.println("\t1. Reach into the Ooze\n\t2. Leave\n\n");
         System.out.print("\tChoose an option: ");
-        int options = scanner.nextInt();
-        if (options == 1) {
+        String options = scanner.next();
+        if (options.equals("1")) {
             player.setCurrentHealth(player.getCurrentHealth() - 3);
             System.out.println("\n\tOuch!\n\tAll you find is corroded metal and a bit of burning pain.");
         }
-        else if (options == 2) {
+        else if (options.equals("2")) {
             System.out.println("\n\tYou decide to leave the area.\n\tThe slime pays no attention, content with its metal");
         }
         else {
