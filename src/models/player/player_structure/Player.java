248b09dc64fe20742a2931dfc7db8c9b985559d9
Frank Rieger <frankrieger@auf.bundeswehr.de>
2024-12-04 11:44:41 +0100

    Refactoring

--- src/models/player/player_structure/Player.java
+++ src/models/player/player_structure/Player.java
@@ -1,9 +1,9 @@
 package models.player.player_structure;
 
+import controller.listener.PlayerEventListener;
 import events.PlayerBlockEvent;
 import events.PlayerDamageEvent;
 import javafx.stage.Stage;
-import controller.listener.*;
 import models.card.card_structure.Card;
 import models.game_settings.GameSettings;
 import models.potion.potion_structure.PotionCard;
@@ -21,108 +21,52 @@ import java.util.List;
  * @author OF Daniel Willig
  */
 public abstract class Player {
-    private Stage primaryStage;
-    // * Variables *
-    private final String name;
-    private String username;
 
-    private int maxHealth;
-    private int currentHealth;
-
-    private int maxEnergy;
-    private int currentEnergy;
-
-    private int gold;
-
-    private int block;
-
-    private List<Card> deck;
-    private List<PotionCard> potionCards;
-
-    private Relic relic;
-
-    private int currentAct;
-
-    private String currentField;
-
-
-    private PlayerType playerType;
-
-    private PlayerEventListener playerEventListener;
-
-    private String imagePath;
     private String altImagePath;
+    private int block;
+    private int currentAct = 1;
+    private int currentEnergy;
+    private String currentField = "0";
+    private int currentHealth;
+    private List<Card> deck;
+    private int gold;
+    private String imagePath;
+    private int maxEnergy;
+    private int maxHealth;
+    private final String name;
+    private PlayerEventListener playerEventListener;
+    private final PlayerType playerType;
+    private List<PotionCard> potionCards;
+    private Stage primaryStage;
+    private Relic relic;
+    private String username;
 
     /**
      * Konstruktor für die Player-Klasse.
      *
-     * @param name Der Name des Characters.
-     * @param maxHealth Die maximale Gesundheit des Spielers.
-     * @param maxEnergy Die maximale Energie des Spielers.
-     * @param playerType Der Typ des Spielers.
+     * @param name         Der Name des Characters.
+     * @param maxHealth    Die maximale Gesundheit des Spielers.
+     * @param maxEnergy    Die maximale Energie des Spielers.
+     * @param playerType   Der Typ des Spielers.
      * @param primaryStage die aktuelle Stage
      */
     public Player(String name, int maxHealth, int maxEnergy, PlayerType playerType, Stage primaryStage) {
         this.name = name;
         this.maxHealth = maxHealth;
-        this.maxEnergy = maxEnergy;
         this.currentHealth = this.maxHealth;
+        this.maxEnergy = maxEnergy;
         this.currentEnergy = this.maxEnergy;
-        this.gold = 0;
-        this.currentAct = 1;
-        this.currentField = "0";
         this.playerType = playerType;
         this.primaryStage = primaryStage;
         this.potionCards = new ArrayList<>();
-        playerEventListener = null;
     }
 
-    public Stage getPrimaryStage() {
-        return primaryStage;
-    }
-
-    public List<PotionCard> getPotionCards() {
-        return potionCards;
-    }
-
-    public void setPotionCards(List<PotionCard> potionCards) {
-        this.potionCards = potionCards;
+    public void addCardToDeck(Card addCard) {
+        this.deck.add(addCard);
     }
 
     public void addPotionCard(PotionCard potionCards) {
         this.potionCards.add(potionCards);
-    }
-
-    public void setPrimaryStage(Stage primaryStage) {
-        this.primaryStage = primaryStage;
-    }
-
-    // * Methods *
-    protected abstract void initDeck();
-
-    protected abstract void initRelic();
-
-    /**
-     * Überprüft, ob der Spieler lebt.
-     *
-     * @return true, wenn die aktuelle Gesundheit größer als 0 ist, andernfalls false.
-     */
-    public boolean isAlive() {
-        return currentHealth > 0;
-    }
-
-    /**
-     * Setzt die aktuelle Energie des Spielers auf die maximale Energie zurück.
-     */
-    public void resetEnergy() {
-        currentEnergy = maxEnergy;
-    }
-
-    /**
-     * Setzt den Blockwert des Spielers auf 0 zurück.
-     */
-    public void resetBlock() {
-        block = 0;
     }
 
     /**
@@ -136,18 +80,9 @@ public abstract class Player {
     }
 
     /**
-     * Erhöht die aktuelle Energie des Spielers um einen bestimmten Betrag.
-     *
-     * @param energy Der Betrag, um den die Energie erhöht werden soll.
-     */
-    public void increaseCurrentEnergy(int energy) {
-        currentEnergy += energy;
-    }
-
-    /**
      * Verringert die aktuelle Gesundheit des Spielers um den angegebenen Schaden.
      *
-     * @param dmg Der Schaden, der dem Spieler zugefügt wird.
+     * @param dmg            Der Schaden, der dem Spieler zugefügt wird.
      * @param damageFromCard Gibt an, ob der Schaden von einer Karte stammt.
      */
     public void decreaseCurrentHealth(int dmg, boolean damageFromCard) {
@@ -172,6 +107,162 @@ public abstract class Player {
     }
 
     /**
+     * Verringert das Gold des Spielers um einen bestimmten Betrag.
+     *
+     * @param gold Der Betrag, um den das Gold verringert werden soll.
+     */
+    public void decreaseGold(int gold) {
+        this.gold -= gold;
+    }
+
+    public String getAltImagePath() {
+        return altImagePath;
+    }
+
+    public void setAltImagePath(String altImagePath) {
+        this.altImagePath = altImagePath;
+    }
+
+    public int getBlock() {
+        return block;
+    }
+
+    public void setBlock(int block) {
+        this.block = block;
+    }
+
+    public int getCurrentAct() {
+        return currentAct;
+    }
+
+    public void setCurrentAct(int currentAct) {
+        this.currentAct = currentAct;
+    }
+
+    public int getCurrentEnergy() {
+        return currentEnergy;
+    }
+
+    public String getCurrentField() {
+        return currentField;
+    }
+
+    public void setCurrentField(String currentField) {
+        this.currentField = currentField;
+    }
+
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public void setCurrentHealth(int currentHealth) {
+        this.currentHealth = currentHealth;
+    }
+
+    public List<Card> getDeck() {
+        return deck;
+    }
+
+    public void setDeck(List<Card> deck) {
+        this.deck = deck;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public int getMaxEnergy() {
+        return maxEnergy;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public void setMaxHealth(int maxHealth) {
+        this.maxHealth = maxHealth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public PlayerEventListener getPlayerEventListener() {
+        return playerEventListener;
+    }
+
+    public void setPlayerEventListener(PlayerEventListener playerEventListener) {
+        this.playerEventListener = playerEventListener;
+    }
+
+    public PlayerType getPlayerType() {
+        return playerType;
+    }
+
+    public List<PotionCard> getPotionCards() {
+        return potionCards;
+    }
+
+    public void setPotionCards(List<PotionCard> potionCards) {
+        this.potionCards = potionCards;
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+    public Relic getRelic() {
+        return relic;
+    }
+
+    public void setRelic(Relic relic) {
+        this.relic = relic;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Erhöht den Blockwert des Spielers um einen bestimmten Betrag.
+     *
+     * @param block Der Betrag, um den der Blockwert erhöht werden soll.
+     */
+    public void increaseBlock(int block) {
+        this.block += block;
+        notifyBlockReceived(block);
+    }
+
+    /**
+     * Erhöht die aktuelle Energie des Spielers um einen bestimmten Betrag.
+     *
+     * @param energy Der Betrag, um den die Energie erhöht werden soll.
+     */
+    public void increaseCurrentEnergy(int energy) {
+        currentEnergy += energy;
+    }
+
+    /**
      * Erhöht die aktuelle Gesundheit des Spielers um einen bestimmten Punktwert.
      *
      * @param hp Der Punktwert, um den die Gesundheit erhöht werden soll.
@@ -181,15 +272,6 @@ public abstract class Player {
         if (currentHealth > maxHealth) {
             currentHealth = maxHealth;
         }
-    }
-
-    /**
-     * Erhöht die maximale Gesundheit des Spielers um einen bestimmten Punktwert.
-     *
-     * @param hp Der Punktwert, um den die maximale Gesundheit erhöht werden soll.
-     */
-    public void increaseMaxHealth(int hp) {
-        maxHealth += hp;
     }
 
     /**
@@ -203,23 +285,44 @@ public abstract class Player {
     }
 
     /**
-     * Verringert das Gold des Spielers um einen bestimmten Betrag.
+     * Erhöht die maximale Gesundheit des Spielers um einen bestimmten Punktwert.
      *
-     * @param gold Der Betrag, um den das Gold verringert werden soll.
+     * @param hp Der Punktwert, um den die maximale Gesundheit erhöht werden soll.
      */
-    public void decreaseGold(int gold) {
-        this.gold -= gold;
+    public void increaseMaxHealth(int hp) {
+        maxHealth += hp;
     }
 
     /**
-     * Erhöht den Blockwert des Spielers um einen bestimmten Betrag.
+     * Überprüft, ob der Spieler lebt.
      *
-     * @param block Der Betrag, um den der Blockwert erhöht werden soll.
+     * @return true, wenn die aktuelle Gesundheit größer als 0 ist, andernfalls false.
      */
-    public void increaseBlock(int block) {
-        this.block += block;
-        notifyBlockReceived(block);
+    public boolean isAlive() {
+        return currentHealth > 0;
     }
+
+    public void removeCardFromDeck(Card addCard) {
+        this.deck.remove(addCard);
+    }
+
+    /**
+     * Setzt den Blockwert des Spielers auf 0 zurück.
+     */
+    public void resetBlock() {
+        block = 0;
+    }
+
+    /**
+     * Setzt die aktuelle Energie des Spielers auf die maximale Energie zurück.
+     */
+    public void resetEnergy() {
+        currentEnergy = maxEnergy;
+    }
+
+    protected abstract void initDeck();
+
+    protected abstract void initRelic();
 
     /**
      * Benachrichtigt den Listener über den empfangenen Blockwert.
@@ -234,7 +337,7 @@ public abstract class Player {
     /**
      * Benachrichtigt den Listener über den empfangenen Schaden.
      *
-     * @param damageAmount Der Betrag des Schadens, der empfangen wurde.
+     * @param damageAmount   Der Betrag des Schadens, der empfangen wurde.
      * @param damageFromCard Gibt an, ob der Schaden von einer Karte stammt.
      */
     protected void notifyDamageReceived(int damageAmount, boolean damageFromCard) {
@@ -243,123 +346,4 @@ public abstract class Player {
 
     }
 
-
-
-    // * Getter & Setter *
-    public List<Card> getDeck() {
-        return deck;
-    }
-
-    public void setDeck(List<Card> deck) {
-        this.deck = deck;
-    }
-
-    public void addCardToDeck(Card addCard) {
-        this.deck.add(addCard);
-    }
-
-    public void removeCardFromDeck(Card addCard) {
-        this.deck.remove(addCard);
-    }
-
-    public String getName() {
-        return name;
-    }
-    public int getMaxHealth() {
-        return maxHealth;
-    }
-
-    public void setMaxHealth(int maxHealth) {
-        this.maxHealth = maxHealth;
-    }
-
-    public int getCurrentHealth() {
-        return currentHealth;
-    }
-
-    public void setCurrentHealth(int currentHealth){
-        this.currentHealth = currentHealth;
-    }
-    public int getMaxEnergy() {
-        return maxEnergy;
-    }
-
-    public int getCurrentEnergy() {
-        return currentEnergy;
-    }
-
-    public int getGold() {
-        return gold;
-    }
-
-    public void setGold(int gold) {
-        this.gold = gold;
-    }
-
-    public int getBlock() {
-        return block;
-    }
-
-    public void setBlock(int block) {
-        this.block = block;
-    }
-
-    public Relic getRelic() {
-        return relic;
-    }
-
-    public void setRelic(Relic relic) {
-        this.relic = relic;
-    }
-
-    public void setCurrentAct(int currentAct){
-        this.currentAct = currentAct;
-    }
-
-    public int getCurrentAct(){
-        return currentAct;
-    }
-
-    public PlayerType getPlayerType() {
-        return playerType;
-    }
-
-    public PlayerEventListener getPlayerEventListener() {
-        return playerEventListener;
-    }
-
-    public void setPlayerEventListener(PlayerEventListener playerEventListener){
-            this.playerEventListener = playerEventListener;
-    }
-    public String getCurrentField() {
-        return currentField;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setCurrentField(String currentField) {
-        this.currentField = currentField;
-    }
-
-    public String getImagePath() {
-        return imagePath;
-    }
-
-    public String getAltImagePath() {
-        return altImagePath;
-    }
-
-    public void setImagePath(String imagePath) {
-        this.imagePath = imagePath;
-    }
-
-    public void setAltImagePath(String altImagePath) {
-        this.altImagePath = altImagePath;
-    }
 }
